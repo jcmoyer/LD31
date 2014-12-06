ff9b3ba2c7ad65943e9c9bb705b4448acf83d3a7
J.C. Moyer <jmoyer1992@gmail.com>
2014-12-06 04:11:03 -0500

    Forward menustate to playstate

--- states/menustate.lua
+++ states/menustate.lua
@@ -1,5 +1,15 @@
 local gamestate = require('hug.gamestate')
+local playstate = require('states.playstate')
 
 local menustate = setmetatable({}, {__index = gamestate})
+local mt = {__index = menustate}
+
+function menustate.new()
+  return setmetatable({}, mt)
+end
+
+function menustate:enter()
+  self:sm():push(playstate.new())
+end
 
 return menustate
